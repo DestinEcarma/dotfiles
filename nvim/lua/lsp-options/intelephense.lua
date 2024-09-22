31281853a47a70df6f0c9ed998a11f73da19ce1c
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-09-23 00:40:28 +0800

    Create intelephense.lua

---
+++ nvim/lua/lsp-options/intelephense.lua
@@ -0,0 +1,14 @@
+return {
+	settings = {
+		intelephense = {
+			telemetry = {
+				enabled = false,
+			},
+			environment = {
+				includePaths = {
+					"[...redacted...]/phpunit/vendor/**",
+				},
+			},
+		},
+	},
+}
