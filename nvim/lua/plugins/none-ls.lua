647fc8ec558ff0b361699e0f4816979b8d8fcf54
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-09-23 00:41:36 +0800

    Update none-ls.lua
    Added `asmfmt` to formatting.

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -14,6 +14,7 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.clang_format,
+				null_ls.builtins.formatting.asmfmt,
 			},
 
 			on_attach = function(client, bufnr)
