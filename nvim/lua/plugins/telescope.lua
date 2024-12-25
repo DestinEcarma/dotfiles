3beabf266b3ab0719b8795c3b8a8872ce7646a92
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-12-25 22:55:58 +0800

    feat: added a find old files to telescope keymap

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -27,6 +27,7 @@ return {
 		vim.keymap.set("n", "<C-P>", builtin.find_files, { desc = "Telescope find files" })
 		vim.keymap.set("n", "<C-F>", builtin.live_grep, { desc = "Telescope live grep" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Telescope oldfiles" })
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 	end,
 }
