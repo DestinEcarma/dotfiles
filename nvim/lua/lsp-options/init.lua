7f975dcdb590c9761e227194e6962029f909b569
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-12-25 14:51:11 +0800

    feat: added descriptions to lsp-options on attach keymaps

--- nvim/lua/lsp-options/init.lua
+++ nvim/lua/lsp-options/init.lua
@@ -1,3 +1,5 @@
+local merge_tables = require("utils.merge-tables")
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 return {
@@ -8,19 +10,34 @@ return {
 
 		local builtin = require("telescope.builtin")
 
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, merge_tables(opts, { desc = "Display Documentation" }))
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, merge_tables(opts, { desc = "Go to Definition" }))
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, merge_tables(opts, { desc = "Go to Declaration" }))
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, merge_tables(opts, { desc = "Go to Implementation" }))
+
+		vim.keymap.set(
+			"n",
+			"<leader>e",
+			vim.lsp.diagnostic.get_line_diagnostics,
+			merge_tables(opts, { desc = "Display Line Diagnostics" })
+		)
+		vim.keymap.set("n", "<leader>ra", vim.lsp.buf.rename, merge_tables(opts, { desc = "Rename" }))
+		vim.keymap.set("n", "<leader>td", vim.lsp.buf.type_definition, merge_tables(opts, { desc = "Type Definition" }))
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, merge_tables(opts, { desc = "Code Action" }))
 
 		-- Telescope
-		vim.keymap.set("n", "<leader>fr", builtin.lsp_references, opts)
-		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, opts)
-		vim.keymap.set("n", "<leader>fq", builtin.quickfix, opts)
-
-		vim.keymap.set("n", "<leader>e", vim.lsp.diagnostic.get_line_diagnostics, opts)
-		vim.keymap.set("n", "<leader>ra", vim.lsp.buf.rename, opts)
-		vim.keymap.set("n", "<leader>td", vim.lsp.buf.type_definition, opts)
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set(
+			"n",
+			"<leader>fr",
+			builtin.lsp_references,
+			merge_tables(opts, { desc = "Telescope LSP References" })
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>fd",
+			builtin.diagnostics,
+			merge_tables(opts, { desc = "Telescope LSP Diagnostics" })
+		)
+		vim.keymap.set("n", "<leader>fq", builtin.quickfix, merge_tables(opts, { desc = "Telescope LSP Quickfix" }))
 	end,
 }
