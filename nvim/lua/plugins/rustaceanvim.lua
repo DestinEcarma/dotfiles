896172aef66f2cb3f0199cbca07ca47891f2c6ea
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-12-25 14:42:20 +0800

    add: rustaceanvim.lua

---
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -0,0 +1,42 @@
+local merge_tables = require("utils.merge-tables")
+
+return {
+	"mrcjkb/rustaceanvim",
+	version = "^5.20.1",
+	lazy = false,
+
+	config = function()
+		vim.g.rustaceanvim = {
+			tools = {
+				hover_actions = {
+					border = "rounded",
+				},
+			},
+
+			server = {
+				on_attach = function(client, bufnr)
+					require("lsp-options").on_attach(client, bufnr)
+
+					local opts = { silent = true, noremap = true, buffer = bufnr }
+
+					local function hover_actions()
+						vim.cmd.RustLsp({ "hover", "actions" })
+					end
+
+					local function code_actions()
+						vim.cmd.RustLsp("codeAction")
+					end
+
+					vim.keymap.set("n", "K", hover_actions, merge_tables(opts, { desc = "Display Documentation" }))
+					vim.keymap.set("n", "<leader>ca", code_actions, merge_tables(opts, { desc = "Code Actions" }))
+				end,
+
+				default_settings = {
+					["rust-analyzer"] = {
+						capabilites = require("lsp-options").capabilities,
+					},
+				},
+			},
+		}
+	end,
+}
