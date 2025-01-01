d8593b3cb105b95caab53a5c76ea459a47ea3b21
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-01-01 14:53:25 +0800

    fix: `rustaceanvim.lua`

--- nvim/lua/plugins/rustaceanvim.lua
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -1,5 +1,9 @@
 local merge_tables = require("utils.merge-tables")
 
+local hover_actions = "<cmd>RustLsp hover actions<CR>"
+local diagnostics = "<cmd>RustLsp renderDiagnostic<CR>"
+local code_actions = "<cmd>RustLsp codeAction<CR>"
+
 return {
 	"mrcjkb/rustaceanvim",
 	version = "^5.20.1",
@@ -8,26 +12,16 @@ return {
 	config = function()
 		vim.g.rustaceanvim = {
 			tools = {
-				hover_actions = {
-					border = "rounded",
-				},
+				float_win_config = { border = "rounded" },
 			},
-
 			server = {
 				on_attach = function(client, bufnr)
 					require("lsp-options").on_attach(client, bufnr)
 
 					local opts = { silent = true, noremap = true, buffer = bufnr }
 
-					local function hover_actions()
-						vim.cmd.RustLsp({ "hover", "actions" })
-					end
-
-					local function code_actions()
-						vim.cmd.RustLsp("codeAction")
-					end
-
 					vim.keymap.set("n", "K", hover_actions, merge_tables(opts, { desc = "Display Documentation" }))
+					vim.keymap.set("n", "<leader>e", diagnostics, merge_tables({ desc = "Display Line Diagnostics" }))
 					vim.keymap.set("n", "<leader>ca", code_actions, merge_tables(opts, { desc = "Code Actions" }))
 
 					vim.api.nvim_create_autocmd("BufWritePre", {
@@ -39,12 +33,6 @@ return {
 
 					vim.keymap.set("n", "<leader>nf", vim.lsp.buf.format, { desc = "Format" })
 				end,
-
-				default_settings = {
-					["rust-analyzer"] = {
-						capabilites = require("lsp-options").capabilities,
-					},
-				},
 			},
 		}
 	end,
