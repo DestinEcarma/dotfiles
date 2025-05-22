96b85e9c94ffab9861b50fd0b1fbbf8bcb9bca2f
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-05-22 13:29:49 +0800

    refactor: change from `prettier` to `prettierd`

--- config/nvim/lua/plugins/none-ls.lua
+++ config/nvim/lua/plugins/none-ls.lua
@@ -10,7 +10,7 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.prettierd,
 				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.formatting.asmfmt,
 				null_ls.builtins.formatting.black.with({
