e1c2ecc82e3b4dec9f6198a20c899c39447de035
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-09-17 20:43:39 +0800

    fix: nvim `rustaceanvim.lua` remove deleted file `lsp-options.lua`

--- config/nvim/lua/plugins/rustaceanvim.lua
+++ config/nvim/lua/plugins/rustaceanvim.lua
@@ -10,7 +10,7 @@ return {
 			},
 			server = {
 				on_attach = function(client, bufnr)
-					require("lsp-options").on_attach(client, bufnr)
+					local builtin = require("telescope.builtin")
 
 					local hover_actions = "<cmd>RustLsp hover actions<CR>"
 					local diagnostics = "<cmd>RustLsp renderDiagnostic<CR>"
@@ -21,12 +21,20 @@ return {
 						{ "K", hover_actions, desc = "Display Documentation" },
 						{ "<leader>e", diagnostics, desc = "Display Line Diagnostics" },
 						{ "<leader>ca", code_actions, desc = "Code Actions" },
+						{ "<leader>ra", vim.lsp.buf.rename, desc = "Rename" },
 
 						{ "<leader>l", group = "LSP" },
+						{ "<leader>ll", require("lsp_lines").toggle, desc = "Toggle LSP Lines" },
 						{ "<leader>lK", hover_actions, desc = "Display Documentation" },
+						{ "<leader>lgd", vim.lsp.buf.definition, desc = "Go to Definition" },
 						{ "<leader>le", diagnostics, desc = "Display Line Diagnostics" },
+						{ "<leader>lra", vim.lsp.buf.rename, desc = "Rename" },
 						{ "<leader>lca", code_actions, desc = "Code Actions" },
 						{ "<leader>lf", vim.lsp.buf.format, desc = "Format" },
+
+						{ "<leader>fr", builtin.lsp_references, desc = "Telescope LSP References" },
+						{ "<leader>fd", builtin.diagnostics, desc = "Telescope LSP Diagnostics" },
+						{ "<leader>fq", builtin.quickfix, desc = "Telescope LSP Quickfix" },
 					})
 
 					if
