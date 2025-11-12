7dd69538b7c7fa48b2622e8fb798436dcb69d07d
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-11-13 06:59:27 +0800

    refactor(nvim): change theme to `catppuccin`

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,10 +1,10 @@
 return {
-	"Mofiqul/dracula.nvim",
-	lazy = false,
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
 
 	config = function()
-		vim.cmd([[colorscheme dracula]])
+		vim.cmd([[colorscheme catppuccin]])
 
 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
