8815f67b39c924da880db44651f21c8ebf77e1a7
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-11-15 06:02:58 +0800

    refactor(nvim): use catppuccin theme and change the design of lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -5,65 +5,29 @@ return {
 	},
 
 	config = function()
-		-- Bubbles config for lualine
-		-- Author: lokesh-krishna
-		-- MIT license, see LICENSE for more details.
-
-		-- stylua: ignore
-		local colors = {
-			blue   = '#80a0ff',
-			cyan   = '#79dac8',
-			black  = '#080808',
-			white  = '#c6c6c6',
-			red    = '#ff5189',
-			violet = '#d183e8',
-			grey   = '#303030',
-		}
-
-		local bubbles_theme = {
-			normal = {
-				a = { fg = colors.black, bg = colors.violet },
-				b = { fg = colors.white, bg = colors.grey },
-				c = { fg = colors.white },
-			},
-
-			insert = { a = { fg = colors.black, bg = colors.blue } },
-			visual = { a = { fg = colors.black, bg = colors.cyan } },
-			replace = { a = { fg = colors.black, bg = colors.red } },
-
-			inactive = {
-				a = { fg = colors.white, bg = colors.black },
-				b = { fg = colors.white, bg = colors.black },
-				c = { fg = colors.white },
-			},
-		}
-
 		require("lualine").setup({
 			options = {
-				theme = bubbles_theme,
-				component_separators = "",
+				theme = "catppuccin",
+				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
 			},
 			sections = {
-				lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+				lualine_a = { "mode" },
 				lualine_b = { "filename", "branch" },
 				lualine_c = {
-					{
-						function()
-							local recording = vim.fn.reg_recording()
+					function()
+						local rec_key = vim.fn.reg_recording()
 
-							if recording ~= "" then
-								return "Recording: " .. recording
-							else
-								return ""
-							end
-						end,
-						color = { fg = "#ff9e64" },
-					},
+						if rec_key ~= "" then
+							return " " .. rec_key
+						end
+
+						return ""
+					end,
 				},
-				lualine_x = {},
-				lualine_y = { "filetype", "progress" },
-				lualine_z = { { "location", separator = { right = "" }, left_padding = 2 } },
+				lualine_x = { "diagnostics" },
+				lualine_y = { "filetype" },
+				lualine_z = { "location", "progress" },
 			},
 			inactive_sections = {
 				lualine_a = { "filename" },
