2b56805ff123b334cb440619e7474d61974a31a7
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-11-15 06:59:09 +0800

    feat(nvim),refactor(nvim): use catppuccin's theme, remove close buffer button, and use slanted separator

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -7,13 +7,11 @@ return {
 		local bufferline = require("bufferline")
 
 		bufferline.setup({
+			highlights = require("catppuccin.special.bufferline").get_theme(),
 			options = {
 				mode = "tabs",
-				hover = {
-					enabled = true,
-					delay = 200,
-					reveal = { "close" },
-				},
+				buffer_close_icon = "",
+				separator_style = "slant",
 				offsets = {
 					{
 						filetype = "neo-tree",
