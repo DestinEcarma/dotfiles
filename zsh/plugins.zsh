01b08841e38db46b921f83cc93badd27c878d579
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2026-07-29 02:08:48 +0800

    feat(zsh): Enhance fzf-tab with improved previews and command integration
    - Integrate `bat` and `chafa` for richer text and image file previews based on MIME types.
    - Improve command completion previews using `tldr`, `man`, and `which`.
    - Add specific completion previews for process management (`kill`, `ps`), `systemd`, `pacman`, and `paru`.
    - Update completion styling for better colorization and matching.

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -11,22 +11,51 @@ zinit wait lucid light-mode for \
 
 _setup_fzf_tab() {
     unfunction _setup_fzf_tab
+
+    # for list-colors
+    eval "$(dircolors -b)"
+
+    zstyle ":completion:*" matcher-list 'm:{a-zA-Z}={A-Za-z}'
+    zstyle ":completion:*" list-colors ${(s.:.)LS_COLORS}
     zstyle ":fzf-tab:*" use-fzf-default-opts yes
-    zstyle ":fzf-tab:*" fzf-flags --preview-window="right:80%"
-    zstyle ":fzf-tab:complete:*:*" fzf-preview "
-    if [[ -d \$realpath ]]; then
-        eza -1 --icons=always --color=always --group-directories-first -- \$realpath
-    elif [[ -f \$realpath ]]; then
-        bat --color=always --style=numbers,grid --line-range=:500 -- \$realpath
+    zstyle ":fzf-tab:*" fzf-flags --preview-window="right:70%"
+
+    zstyle ":fzf-tab:complete:*:*" fzf-preview '
+    mime=$(file -bL --mime-type "$realpath")
+    category=${mime%%/*}
+    if [[ -d $realpath ]]; then
+        eza -1 --icons=always --color=always --group-directories-first $realpath
+    elif [[ "$category" = text ]]; then
+        bat --color=always --style=plain --line-range=:500 $realpath
+    elif [[ "$category" = image ]]; then
+        chafa $realpath
     else
-        {
-            tldr -m \$word 2>/dev/null ||
-                man -m \$word 2>/dev/null ||
-                echo \"No documentation available\"
+        echo ${(P)word}
+    fi'
+    zstyle ":fzf-tab:complete:*:options" fzf-preview
+    zstyle ":fzf-tab:complete:*:argument-1" fzf-preview
+
+    # commands
+    zstyle ":fzf-tab:complete:-command-:*" fzf-preview '
+    {
+        tldr -m "$word" 2>/dev/null ||
+            man -m "$word" 2>/dev/null ||
+            which "$word"
         } | CLICOLOR_FORCE=1 COLORTERM=truecolor glow -s dark
-    fi"
-    zstyle ":fzf-tab:complete:*:options" fzf-preview ""
-    zstyle ":fzf-tab:complete:*:argument-1" fzf-preview ""
+    '
+
+    # kill|ps
+    zstyle ":completion:*:*:*:*:processes" command "ps -u $USER -o pid,user,comm -w -w"
+    zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-preview \
+        '[[ $group == "[process ID]" ]] && ps --pid="$word" -o cmd --no-headers -w -w'
+    zstyle ":fzf-tab:complete:(kill|ps):argument-rest" fzf-flags --preview-window=down:3:wrap
+
+    # systemd
+    zstyle ":fzf-tab:complete:systemctl:*" fzf-preview
+    zstyle ":fzf-tab:complete:systemctl-*:*" fzf-preview 'SYSTEMD_COLORS=1 systemctl status "$word"'
+
+    # pacman|paru
+    zstyle ":fzf-tab:complete:(pacman|paru):*" fzf-preview 'pacman -Si ${(Q)word}'
 
     eval "$(fzf --zsh)"
 }
